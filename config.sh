8196fc5286f9e6ed86364f8734aec26dae63e128
AZ technology <aztec@localhost.domain>
2015-02-16 14:42:56 +0800

    Change default git repo to fling2
    Change-Id: I5950e2653c8932c8c6aee0933967bcf1e0bc1750

--- config.sh
+++ config.sh
@@ -30,7 +30,7 @@ case `uname` in
 	exit -1
 esac
 
-GITREPO=${GITREPO:-"git://github.com/flingone/b2g-manifest"}
+GITREPO=${GITREPO:-"git://github.com/fling2/b2g-manifest"}
 BRANCH=${BRANCH:-master}
 
 while [ $# -ge 1 ]; do
